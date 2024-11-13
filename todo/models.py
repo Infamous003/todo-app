79a024a6242e06a7face8cd289ad70364a9b21f3
Infamous003 <arfathsyed0312@gmail.com>
2024-11-13 17:24:32 +0530

    Add Task table to store all the tasks

--- todo/models.py
+++ todo/models.py
@@ -2,6 +2,7 @@ from todo import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from todo import login_manager
 from flask_login import UserMixin
+from datetime import datetime
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -13,6 +14,8 @@ class User(db.Model, UserMixin):
     name = db.Column(db.String(64), nullable=False)
     email = db.Column(db.String(64), unique=True, nullable=False)
     password_hash = db.Column(db.String(64), nullable=False)
+
+    tasks = db.relationship("Task", backref="owned_task", lazy=True)
 
     @property
     def password():
@@ -27,3 +30,14 @@ class User(db.Model, UserMixin):
     
     def get_id(self):
         return (self.user_id)
+    
+class Task(db.Model):
+    task_id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(64), nullable=False, unique=True)
+    status = db.Column(db.Boolean, default=False, unique=False)
+    created_at = db.Column(db.DateTime, default=datetime.now)
+
+    user_id = db.Column(db.Integer(), db.ForeignKey("user.user_id"))
+
+    def __repr__(self):
+        return f"task_id: {self.task_id} | name: {self.name} | status: {self.status} | created_at: {self.created_at}\n"
