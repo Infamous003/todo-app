d5793df3806d43cfe7c37d359f54b127970e3470
Infamous003 <arfathsyed0312@gmail.com>
2024-12-13 22:03:48 +0530

    Change PORT number

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from todo import app
 import os
-PORT = int(os.environ.get("PORT", 5000))
+PORT = int(os.environ.get("PORT", 8080))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=PORT)
